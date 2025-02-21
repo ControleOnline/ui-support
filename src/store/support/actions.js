60be64c8ba8ece9ec4aa42f9cbdc11302116e3bd
Luiz Kim <luiz.kim@controleonline.com>
2025-02-21 11:04:20 -0300

    Update

--- src/store/support/actions.js
+++ src/store/support/actions.js
@@ -1,28 +1,29 @@
-import { api } from "@controleonline/../../src/boot/api";
+import { api } from "@controleonline/ui-common/src/api";
+
 import * as types from "@controleonline/ui-default/src/store/default/mutation_types";
 
 export const getNews = ({ commit }, params = {}) => {
   commit(types.SET_ISLOADING);
 
-  return api.fetch('/support/news', { params })
-    
-    .then(data => {
+  return api
+    .fetch("/support/news", { params })
+
+    .then((data) => {
       commit(types.SET_ISLOADING, false);
 
       return data.response ? data.response : null;
-
     });
 };
 
 export const getFaqs = ({ commit }, params = {}) => {
   commit(types.SET_ISLOADING);
 
-  return api.fetch('/support/faqs', { params })
-    
-    .then(data => {
+  return api
+    .fetch("/support/faqs", { params })
+
+    .then((data) => {
       commit(types.SET_ISLOADING, false);
 
       return data.response ? data.response : null;
-
     });
 };
