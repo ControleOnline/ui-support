0d02c94bc489dcd7e8ce5b94416adbd59ff8b6fd
Luiz Kim <luiz.kim@controleonline.com>
2025-03-11 16:41:21 -0300

    Update

--- src/vue/router/routes.js
+++ src/vue/router/routes.js
@@ -1,7 +1,7 @@
 export const routes = [
   {
     path     : '/support/',
-    component: () =>  import ('@controleonline/ui-layout/src/layouts/AdminLayout.vue'),
+    component: () =>  import ('@controleonline/ui-layout/src/vue/layouts/AdminLayout.vue'),
     children : [
       { name: 'FAQ',  path: 'faq',  component: () =>  import ('../pages/Support/FAQ.vue') },
       { name: 'News', path: 'news', component: () =>  import ('../pages/Support/News.vue') },
